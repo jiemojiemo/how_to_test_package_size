42e5b60ba236459b614c0bd0e9f060a5c253ddb0
huawei.william <huawei.william@bytedance.com>
2021-11-21 07:13:32 +0800

    add -DANDROID_STL=c++_shared

--- android_build.sh
+++ android_build.sh
@@ -3,6 +3,7 @@ ANDROID_NDK_HOME=/Users/bytedance/android-ndk-r21e
 cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
       -DANDROID_ABI=arm64-v8a \
       -DBUILD_SHARED_LIBS=ON \
+      -DANDROID_STL=c++_shared \
       -S . -B android_64_build
 
 cmake --build android_64_build -- -j 10
